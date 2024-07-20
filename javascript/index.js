0fe2cbae4bb32c51b36438510e2818466c6a1941
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-07-20 19:36:33 +0200

    added functionality for the select
    added id and value to html
    
    js: added function for time and date display, added function for time and date for drop-down cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,44 @@
 function updateTime() {
   let RomeElement = document.querySelector("#rome");
-  let RomeDateElement = RomeElement.querySelector(".date");
-  let RomeTimeElement = RomeElement.querySelector(".time");
-  let RomeTime = moment().tz("Europe/Rome");
-  RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
-  RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]a[</small>]");
-
+  if (RomeElement) {
+    let RomeDateElement = RomeElement.querySelector(".date");
+    let RomeTimeElement = RomeElement.querySelector(".time");
+    let RomeTime = moment().tz("Europe/Rome");
+    RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
+    RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]a[</small>]");
+  }
   let SydneyElement = document.querySelector("#sydney");
-  let SydneyDateElement = SydneyElement.querySelector(".date");
-  let SydneyTimeElement = SydneyElement.querySelector(".time");
-  let SydneyTime = moment().tz("Australia/Sydney");
-  SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
-  SydneyTimeElement.innerHTML = SydneyTime.format(
-    "h:mm:ss [<small>]a[</small>]"
-  );
+  if (SydneyElement) {
+    let SydneyDateElement = SydneyElement.querySelector(".date");
+    let SydneyTimeElement = SydneyElement.querySelector(".time");
+    let SydneyTime = moment().tz("Australia/Sydney");
+    SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
+    SydneyTimeElement.innerHTML = SydneyTime.format(
+      "h:mm:ss [<small>]a[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = citiesElement = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">
+        ${cityTime.format("h:mm:ss")}
+        <small>${cityTime.format("A")}</small>
+      </div>
+    </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#drop-down");
+citiesSelectElement.addEventListener("change", updateCity);
