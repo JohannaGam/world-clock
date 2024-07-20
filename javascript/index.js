3830dc4142f43c50df377ba550130e848a22a4f6
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-07-20 18:35:54 +0200

    add javascript current date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
-//alert("Still coding ☺");
+function updateTime() {
+  let RomeElement = document.querySelector("#rome");
+  let RomeDateElement = RomeElement.querySelector(".date");
+  let RomeTimeElement = RomeElement.querySelector(".time");
+  let RomeTime = moment().tz("Europe/Rome");
+  RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
+  RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]a[</small>]");
+
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment().tz("Australia/Sydney");
+  SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
